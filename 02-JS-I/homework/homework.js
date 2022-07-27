189282f51a5d40e8ae1d496748748d41d2e0dead
ngpalma <nicolasgerardopalma@gmail.com>
2022-07-27 18:19:39 -0300

    homework JS III terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -26,7 +26,7 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  // "Return" la string provista: str
+  // "Return" la string provista: "Henry"
   // Tu código:
 return 'Henry'
 }
