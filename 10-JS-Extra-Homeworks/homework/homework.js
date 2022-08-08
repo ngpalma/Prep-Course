c9e4c1553039e9777ed9141200ccf0bffcf4688c
ngpalma <nicolasgerardopalma@gmail.com>
2022-08-08 20:20:38 -0300

    Se subieron las explicaciones

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,10 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = []
+  for (let clave in objeto) {
+    matriz.push([clave, objeto[clave]])
+  } return matriz;
 }
 
 
@@ -18,6 +22,7 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  
 }
 
 
